8f9e16dafb0fdb98f078042aac78458693958eee
saddammd <saddam.md@gmail.com>
2022-01-19 21:00:56 +0530

    aa

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, '/dist/nttdbs-poc1')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname,'dist/nttdbs-poc1/index.html'));
+app.all('*', (req,res)=> {
+res.status(200).sendFile(__dirname,'/dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
