fb3f9289fc852c2a31c6bbf0150c505380b50e03
saddammd <saddam.md@gmail.com>
2022-01-19 20:05:16 +0530

    aaa

--- server.js
+++ server.js
@@ -7,13 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(path.join(__dirname, '/dist/nttdbs-poc1')));
 
-app.get('*', (req, res) => {
-    res.sendFile(`./dist/nttdbs-poc1/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname,'dist/nttdbs-poc1/index.html'));
 });
-
-//app.get('/*', function(req,res) {
-//res.sendFile(path.join(__dirname,'dist','nttdbs-poc1','index.html'));
-//});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
